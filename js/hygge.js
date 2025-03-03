377271b148194839f3aacc97d122c7d97036f2ab
cecilieelkjaer24 <eaa24cebj@students.eaaa.dk>
2025-03-03 12:34:05 +0100

    Create hygge.js

---
+++ js/hygge.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function toggleText() {
+    // Find tekstboksen
+    var textElement = document.querySelector('.gemttekst');
+    
+    // Toggle (skift) synlighed
+    textElement.classList.toggle('visible');
+}
