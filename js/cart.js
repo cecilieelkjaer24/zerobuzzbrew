8b4f74e120b17a342364c4cb8a973d6634c9821a
cecilieelkjaer24 <eaa24cebj@students.eaaa.dk>
2025-03-05 09:31:15 +0100

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -15,3 +15,16 @@ closeButton.addEventListener('click', function () {
     cartTab.classList.remove('active'); // Hide cart
     cartTab.classList.add('inactive');  // Explicitly mark inactive (optional)
 });
+
+//slideshow til forsiden 
+//her er der brugt en array til at skifte mellem billederne
+let slides = ["img/hygge-ollen.png", "img/luksus-ollen2.png", "img/natur-ollen3.png"];
+//her er der brugt en variabel til at skifte mellem billederne
+let currentSlide = 0;
+//her bruges der const til at finde id'et i html'en
+const ImgElement = document.getElementById("hygge");
+
+//herunder ses at vi kalder på ide'et til højre og venstre pil og tilføjer en eventlistener for at skifte mellem billederne ved at bruge en funktion
+document.getElementById("venstrepil").addEventListener("click", () => changeSlide(-1));
+document.getElementById("højrepil").addEventListener("click", () => changeSlide(1));
+
