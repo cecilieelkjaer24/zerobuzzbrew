512cb7a95c6574f2b32a80c5d3e0011f08aeb107
cecilieelkjaer24 <eaa24cebj@students.eaaa.dk>
2025-03-05 10:02:29 +0100

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -18,13 +18,21 @@ closeButton.addEventListener('click', function () {
 
 //slideshow til forsiden 
 //her er der brugt en array til at skifte mellem billederne
-let slides = ["img/hygge-ollen.png", "img/luksus-ollen.png", "img/natur-ollen.png"];
+let slides = [
+   {image: "img/hygge-ollen.png", color: "#1b26b2", buttonColor: "#1b26b2", buttonTextColor: "#ccdcff", link: "hygge.html"},
+   {image: "img/luksus-ollen.png", color: "#1b26b2", buttonColor: "#ccdcff", buttonTextColor: "#1b26b2", link: "luksus.html"},
+   {image: "img/natur-ollen.png", color: "#64ac3c", buttonColor: "#64ac3c", buttonTextColor: "#dbf6d6", link: "natur.html"}
+];
+
+
+
 //her er der brugt en variabel til at skifte mellem billederne
 let currentSlide = 0;
 //her bruges der const til at finde id'et i html'en
 const ImgElement = document.getElementById("hygge");
 const titleElement = document.getElementById("title");
 const buttonElement = document.getElementById("laesMereKnap");
+
 
 //herunder ses at vi kalder på ide'et til højre og venstre pil og tilføjer en eventlistener for at skifte mellem billederne ved at bruge en funktion
 document.getElementById("venstrepil").addEventListener("click", () => changeSlide(-1));
@@ -34,6 +42,18 @@ function changeSlide(direction){
     currentSlide += direction;
     if(currentSlide >= slides.length) currentSlide = 0;
     if(currentSlide < 0) currentSlide = slides.length - 1;
-    ImgElement.src = slides[currentSlide];
+
+    ImgElement.src = slides[currentSlide].image;
+    titleElement.style.color = slides[currentSlide].color;
+    buttonElement.style.backgroundColor = slides[currentSlide].buttonColor;
+    buttonElement.style.color = slides[currentSlide].buttonTextColor;
     
+    buttonElement.onclick = function(){
+        window.location.href = slides[currentSlide].link;
+    }
+
+    ImgElement.onclick = function(){
+        window.location.href = slides[currentSlide].link;
+    }
+
 }
